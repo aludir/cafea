dcc0b89f82d6627e55c0296e14a398e28b397010
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-05-04 23:16:50 +0200

    Redirecting the user to the edit profile page after successful sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private
+  def after_sign_in_path_for(resource)
+    if current_user.name.nil?
+      edit_user_path(current_user)
+    else
+      users_path
+    end
+  end
 end

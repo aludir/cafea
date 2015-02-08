47c0681d78591f9e201c1b09fd6eb760b81a0dd3
Tassos 'nakano' Natsakis <github@natsakis.com>
2015-02-08 19:44:53 +0100

    Adding a counter showing how many announcements are there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :count_announcements
+
+  def count_announcements
+    if current_user.present?
+      @announce_count = Announcement.all.count
+    end
+  end
+
   private
   def after_sign_in_path_for(resource)
     if current_user.name.nil?

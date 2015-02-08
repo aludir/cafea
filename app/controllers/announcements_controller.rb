ff5dbbcb4e2b0d6673f4056ecfea691e5faeee6a
Tassos 'nakano' Natsakis <github@natsakis.com>
2015-02-08 20:20:59 +0100

    Adding the new and show announcement views and methods

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,8 +1,22 @@
 class AnnouncementsController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
   def index
   	@announcements = Announcement.all
+  end
+
+  def new
+  	@announcement = Announcement.new
+  end
+  def show
+  	@announcement = Announcement.find_by_id(params[:id])
+  	@user = @announcement.user
+  end
+
+  def create
+  	@announcement = Announcement.new(user_id: current_user.id, title: params[:announcement][:title], body: params[:announcement][:body])
+  	@announcement.save!
+
+  	redirect_to announcements_path
   end
 end

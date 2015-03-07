e9ffd0dacd9f3b377a793e27f30b60da8979a15d
Tassos (nakano) Natsakis <github@eipieq.com>
2015-03-07 20:44:26 +0100

    Showing only unique unique tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,9 @@
 class TagsController < ApplicationController
 	respond_to :json
 	def index
-		tags = Tag.all
+	  
+	  # Making sure that only unique tags are shown in the typeahead
+		tags = Tag.all.map { |k| k[:name]}.uniq.map {|v| Hash[:name, v]}
 		respond_with(tags) do |format|
 			format.json{render :json => tags.as_json(:only => [:name])}
 		end

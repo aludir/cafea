8054ae704a2c7ef297eb534a9a5bfad1f3a7cd0a
Oana Sipos <oana.sipos@up-nxt.com>
2015-03-31 15:08:11 +0200

    First draft of search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,15 @@
 class SearchController < ApplicationController
 
   def show
-  @users = Search.for(params[:id_search], params[:name], 
-                      params[:surname], params[:old_surname], 
+  @users = Search.for(params[:id_search], params[:name],
+                      params[:surname], params[:old_surname],
                       params[:nickname]
                       )
   end
 
   def index
     if params[:search]
-      @users = User.search(params[:search]).order("created_at DESC")
+      @users = Search.for(params[:search]).limit(20)
     else
       @users = User.all.order('created_at DESC')
     end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,23 +1,98 @@
-class Search < ActiveRecord::Base
-  
-  def users
-    @users ||= find_users
+class Search < Struct.new(:query, :options)
+  include PgSearch
+
+  DEFAULT_SEARCH_ON = [:name, :languages]
+
+  # convinience method
+  def self.for(*args)
+    new(*args).call
   end
 
-  def self.for(id_search, name, surname, old_surname, nickname)
+  def initialize(query, options = {})
+    super
+  end
 
-    # escape for security
-    name        = "%#{name}%"
-    surname     = "%#{surname}%"
-    old_surname = "%#{old_surname}%"
-    nickname    = "%#{nickname}%"
+  def call
+    call_pg
+  end
 
-    users = User.order(:name)
-    users = users.where(id: id_search) if id_search.present?
-    users = users.where("name like ?", "%#{name}%") if name.present?
-    users = users.where("surname like ?", "%#{surname}%") if surname.present?
-    users = users.where("old_surname like ?", "%#{old_surname}%") if old_surname.present?
-    users = users.where("nickname like ?", "%#{nickname}%") if nickname.present?
+  def scope_for_search
+    build_search_scope
+    @scope_for_search
+  end
+
+  # This builder and its methods should be extracted to
+  # SearchQueryBuilder
+  def build_search_scope
+    return if @scope_for_search
+
+    parts = options.fetch(:on) { DEFAULT_SEARCH_ON }
+
+    @scope_for_search = {
+      associated_against: {},
+      against: Set.new
+    }
+
+    parts.each do |part|
+      add_against(*pg_scope_for(part))
+    end
+  end
+
+  def pg_scope_for(part)
+    case part
+    when :name
+      [
+        [:name, :surname, :old_surname]
+      ]
+    when :surname
+      [
+        [:surname, :old_surname]
+      ]
+    when :languages
+      [
+        [:name],
+        on: :languages
+      ]
+    else
+      fail "Unknown part #{part} used in search"
+    end
+  end
+
+private
+
+  def add_against(attributes, options = {})
+    attributes = prioritized_attributes(attributes, options[:on])
+    if options[:on]
+      # this implementation only allows for one scope part per association
+      # Want to chnage this? Make sure to merge here or something
+      @scope_for_search[:associated_against][options[:on]] = attributes
+    else
+      @scope_for_search[:against].merge(attributes)
+    end
+  end
+
+  def prioritized_attributes(attributes, association)
+    attributes.map do |attribute_name|
+      [attribute_name, priority_for(attribute_name, association)]
+    end
+  end
+
+  # Implemet weighting here. If you want priority per part,
+  # do it on that level and pass the priority directly to the add_against
+  # method.
+  def priority_for(attribute_name, association)
+    %w(A B C D).sample
+  end
+
+  def call_pg
+    ScopeOptions.new(
+      Configuration.new(
+        {
+          query: query
+        }.merge!(scope_for_search),
+        User
+      )
+    ).apply(User)
   end
 
 end

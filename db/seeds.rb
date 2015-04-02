f1735e33779bc3f2a3fa2702ce2e44069020669e
Oana Sipos <oana.sipos@up-nxt.com>
2015-04-02 23:26:52 +0200

    First draft of seeds. And gems.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Tag.create(name: 'BEST')
-Tag.create(name: 'Travelling')
-Tag.create(name: 'Fun')
-Tag.create(name: 'Business')
+User.delete_all
+User.create(id: '1', email: 'john.doe@email.com', password: 'password',
+            password_confirmation: 'password', name: 'John', surname: 'Doe',
+            birth_date: Time.now, uuid: '1234')
+
+Email.delete_all
+Email.create(id: '1', user_id: '1', address: 'john.doe@email.com',
+             created_at: Time.now)
+Email.create(id: '2', user_id: '1', address: 'john.doe@work.com',
+             created_at: Time.now)
+
+Address.delete_all
+Address.create(id: '1', user_id: '1', street: 'Streetstraat', number: '42', zip: '94610',
+               city: 'Hitchhike', country: 'Hikeland',
+               created_at: Time.now)
+
+Phone.delete_all
+Phone.create(id: '1', user_id: '1', number: '0800800800', created_at: Time.now)
+
+Language.delete_all
+Language.create(id: '1', user_id: '1', name: 'Bringlish', level: 'native',
+                created_at: Time.now)
+
+Education.delete_all
+Education.create(id: '1', user_id: '1', school: 'primary', start_date: Time.now,
+                 end_date: Time.now, field: 'hitchikereeing', grade: '1',
+                 created_at: Time.now)
+
+Lbg.delete_all
+Lbg.create(id: '1', name: 'iamtheBEST', created_at: Time.now)
+
+Announcement.delete_all
+Announcement.create(id: '1', user_id: '1', title: 'My first announcement',
+                    body: 'I want to travel to Japan. Can somebody water my
+                          plants while I am away? Ah, and maybe a bed in JP?',
+                    created_at: Time.now)
+
+Comment.delete_all
+Comment.create(id: '1', user_id: '1', body: 'If you have orchids, they do not need water!',
+                announcement_id: '1', created_at: Time.now)
+
+Tag.delete_all
+Tag.create(id: '1', name: 'BEST', created_at: Time.now)
+Tag.create(id: '2', name: 'Travelling', created_at: Time.now)
+Tag.create(id: '3', name: 'Fun', created_at: Time.now)
+Tag.create(id: '4', name: 'Business', created_at: Time.now)

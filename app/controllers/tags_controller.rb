2a59bcd3619c8daf57ffd6df4669e4378daec555
Tassos (nakano) Natsakis <github@eipieq.com>
2015-04-05 19:19:32 +0200

    Reconstructing the tags json output to the format that Select2 likes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,10 +2,12 @@ class TagsController < ApplicationController
 	respond_to :json
 	def index
 	  
-	  # Making sure that only unique tags are shown in the typeahead
-		tags = Tag.all.map { |k| k[:name]}.uniq.map {|v| Hash[:name, v]}
+	  tags = Array.new
+    Tag.all.each do |t|
+      tags << Hash[:id, t[:id],:text, t[:name]]
+    end
 		respond_with(tags) do |format|
-			format.json{render :json => tags.as_json(:only => [:name])}
+			format.json{render :json => tags.as_json(:only => [:id, :text])}
 		end
 	end
 end

6e53f758cf68b253157c71dcba6306407530a638
Tassos (nakano) Natsakis <github@eipieq.com>
2015-04-23 12:44:30 +0200

    Keeping the tag filtering active when sorting the columns of announcements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {sort: column, direction: direction}, {class: css_class}
+    tag = params[:tag]
+    link_to title, {sort: column, direction: direction, tag: tag}, {class: css_class}
   end
   
   def flash_class(level)

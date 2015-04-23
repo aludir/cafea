71efa4d4122f95b2956bd81085a35080f9a452bb
Tassos (nakano) Natsakis <github@eipieq.com>
2015-04-23 12:50:34 +0200

    Showing better the interests that the user is participating in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(:first, :conditions => ["uuid = ? ", params[:id]])
+    @interests = @user.interests
+    @own_interests = Interest.find(:all, :conditions => ["initiator = ? ", @user.id])
   end
 
   private

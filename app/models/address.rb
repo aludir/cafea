39006647d98404dc8d6b1dc1fb9a1d711ff4742a
Tassos (nakano) Natsakis <github@eipieq.com>
2015-05-01 14:40:26 +0200

    Creating partial view for creating all the user details tables, avoiding code duplication

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ActiveRecord::Base
   belongs_to :user
   belongs_to :country
   belongs_to :city
+  
+  def full_address
+    [self.street,self.number,self.city.name,self.zip,self.country.name].to_sentence(:last_word_connector => ', ', :two_words_connector => ', ').html_safe
+  end
 end

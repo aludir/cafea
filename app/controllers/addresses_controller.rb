faae1cf1092b1de053cc69a3cec2291521137dae
Tassos (nakano) Natsakis <github@eipieq.com>
2015-05-07 12:02:16 +0200

    Made possible to add new countries if they don't exist

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,20 +3,19 @@ class AddressesController < ApplicationController
   before_action only: [:edit, :destroy] do
     user_owns_resource?(Address)
   end
-  before_action only: [:create] do
-    add_user_profile_resource(Address,address_params)
-  end
   
   def create
-    
+    add_user_profile_resource(Address,address_params)
   end
   
   def destroy
     destroy_user_profile_resource(@resource)
   end
   
-  private
+  private 
   def address_params
+    params[:address][:country_id]=Country.find_or_create_by_name(params[:address][:country_id]).id
+    params[:address][:city_id]=City.find_or_create_by_name(params[:address][:city_id]).id
     params.require(:address).permit(:user_id, :street, :number, :city_id, :zip, :country_id, :description)
   end
 end

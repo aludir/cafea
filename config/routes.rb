325539ac974c0f83a1b050cb9b71d8a8e557059b
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2017-10-13 16:18:06 +0300

    Fixing error with naming convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "search/index"
+  get "search/show"
+  devise_for :users, controllers: {registrations: "users/registrations"}
+  
+  root "search#index"
+
+  resources :search, only: [:index, :show]
+  resources :interests
+  post 'interest/:id/join', to: 'interests#join', as: 'join_interest'
+  post 'interest/:id/leave', to: 'interests#leave', as: 'leave_interest'
+  resources :lbgs
+  post 'lbg/:id/join', to: 'lbgs#join', as: 'join_lbg'
+  post 'lbg/:id/leave', to: 'lbgs#leave', as: 'leave_lbg'
+
+  resources :users, only: [:index, :show]
+  resources :announcements
+  get 'announcements?tag=:id', to: 'announcements#index', as: 'announcements_tag'
+  
+  resources :tags, only: [:index]
+  resources :contacts, except: [:index, :show]
+  resources :addresses, except: [:index, :show]
+  resources :languages, except: [:index, :show]
+  resources :educations, except: [:index, :show]
+  resources :experiences, except: [:index, :show]
+  resources :lbgs, except: [:index, :show]
+    
+  post 'comment', to: 'announcements#add_comment', as: 'add_comment'
+  delete 'comment/:id', to: 'announcements#del_comment', as: 'del_comment'
 end

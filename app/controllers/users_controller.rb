2afdc4438910a2eadc71ceb2e7c1704ffb2bc031
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2017-10-31 19:11:56 +0200

    Show the selected user information, not the current one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: :show
 
   # GET /users
   # GET /users.json
@@ -11,8 +12,12 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = current_user
     @interests = @user.interests
     @own_interests = Interest.where(initiator: @user.id)
+  end
+
+  private
+  def set_user
+    @user = User.find_by(uuid: params[:id])
   end
 end
